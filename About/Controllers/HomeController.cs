9e7f3f22f0a248d337ea78c999a0554510269010
arwenv <arwenvh@gmail.com>
2013-06-18 19:44:32 -0400

    started on home page

---
+++ About/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+
+namespace About.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Software developer. More content here.";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Description of Arwen's site here.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact information here.";
+            return View();
+        }
+
+    }
+}
